71b1c8cb510673f3028869fc92a88a65a5bb5bbe
Kevin Campos Buendia <91388417+AlphaSKC@users.noreply.github.com>
2024-03-02 22:43:47 -0500

    FT: CREATE LOG WITH CREATE CATEGORY
    Se crea el registro del log al realizar la creación de una categoria.

--- src/Infraestructure/EventHandlers/Categoria/CreateCategoriaHandler.cs
+++ src/Infraestructure/EventHandlers/Categoria/CreateCategoriaHandler.cs
@@ -3,6 +3,9 @@ using ApplicationCore.Wrappers;
 using Infraestructure.Persistence;
 using AutoMapper;
 using MediatR;
+using ApplicationCore.Interfaces;
+using System.Text.Json;
+using ApplicationCore.DTOs.Log;
 
 namespace Infraestructure.EnventHandlers.Categoria
 {
@@ -10,23 +13,33 @@ namespace Infraestructure.EnventHandlers.Categoria
     {
         private readonly ApplicationDbContext _context;
         private readonly IMapper _mapper;
+        private readonly IDashboardService _service;
 
-        public CreateCategoriaHandler(ApplicationDbContext context, IMapper mapper)
+        public CreateCategoriaHandler(ApplicationDbContext context, IMapper mapper, IDashboardService service)
         {
             _context = context;
             _mapper = mapper;
+            _service = service;
 
         }
 
         public async Task<Response<int>> Handle(CreateCategoriaCommand request, CancellationToken cancellationToken)
         {
-            var u = new CreateCategoriaCommand();
-            u.Nombre = request.Nombre;
+            var c = new CreateCategoriaCommand();
+            c.Nombre = request.Nombre;
 
-            var us = _mapper.Map<Domain.Entities.Categoria>(u);
-            await _context.categorias.AddAsync(us);
+            var json = JsonSerializer.Serialize(c);
+            var log = new LogDto();
+            log.datos =json;
+            log.fecha = DateTime.Now;
+            log.response = "200";
+            log.nombreFuncion = "CreateCategoria()";
+            await _service.CreateLog(log);
+
+            var ca = _mapper.Map<Domain.Entities.Categoria>(c);
+            await _context.categorias.AddAsync(ca);
             await _context.SaveChangesAsync();
-            return new Response<int>(us.pkCategoria, "Registro Creado");
+            return new Response<int>(ca.pkCategoria, "Registro Creado");
 
         }
     }
--- src/Infraestructure/obj/Debug/net6.0/Infraestructure.AssemblyInfo.cs
+++ src/Infraestructure/obj/Debug/net6.0/Infraestructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d12619a0e4ab68d323b2306c36c9c62df3993c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f32605310b469ee4bbe550e1f02827cb576f863c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
